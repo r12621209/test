eea5dddf0d5b388929b4486c49925851a8f5dd6f
r12621209 <163800292+r12621209@users.noreply.github.com>
2025-12-03 21:57:36 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
- def test_export_report_1():
-      a = 1 + 1
-      b = 2 + 2
+def test_export_report_1():
+     a = 1 + 1
+     b = 2 + 2
 
-      assert b > a
+     assert b > a
 
 
-  def test_export_report_2():
-    a = 2 + 2
-    b = 4 + 4
+ def test_export_report_2():
+   a = 2 + 2
+   b = 4 + 4
 
-    assert a < b
+   assert a < b
